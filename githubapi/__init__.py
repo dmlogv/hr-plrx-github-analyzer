61a44cbead7c0be50b4be285ff3334d1f64ce15e
logvinenko_da <logvinenko_da@magnit.ru>
2018-11-28 17:58:52 +0300

    Add GitHub API mock classes

---
+++ githubapi/__init__.py
@@ -0,0 +1,37 @@
+"""
+GitHub Demo API
+"""
+
+__version__ = '0.0.1'
+
+
+# Root GitHub URL (https://developer.github.com/v3/#current-version)
+ROOT = 'https://api.github.com'
+
+
+class Repo:
+    """
+    Repository API
+    """
+    pass
+
+
+class Contributor:
+    """
+    Repository Contributor API
+    """
+    pass
+
+
+class Pull:
+    """
+    Repository Pull-request API
+    """
+    pass
+
+
+class Issue:
+    """
+    Repository Issue API
+    """
+    pass
