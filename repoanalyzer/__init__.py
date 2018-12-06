fa2b70d236e09514a08c40d4c7371d3628e61613
Dmitry Logvinenko <5diezfun@gmail.com>
2018-12-06 23:43:13 +0300

    Add base Report class
    Provides base fields and output methods.

--- repoanalyzer/__init__.py
+++ repoanalyzer/__init__.py
@@ -7,4 +7,58 @@ __version__ = '0.0.1'
 
 
 class Report:
-    pass
+    """
+    Repository analysis and reporting
+    """
+    name = 'Main report'
+    headers = ()
+
+    def __init__(self, repo):
+        self.repo = repo
+        self.results = []
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} "{self.name}" for {self.repo}>'
+
+    def analyze(self, *args, **kwargs):
+        """
+        Analyze repository
+
+        Args:
+            *args, **kwargs: analysis options
+
+        Returns:
+            Report
+        """
+        return self
+
+    def table(self):
+        """
+        Get results in tabular format
+
+        Returns:
+            str
+        """
+        return (
+            f'{self.name}\n' +
+            '-' * len(self.name) + '\n' +
+            '\t'.join(map(str, self.headers)) + '\n' +
+            '\n'.join(['\t'.join(map(str, row)) for row in self.results]) + '\n\n'
+            )
+
+    def json(self):
+        """
+        Get result in JSON-able format
+
+        Returns:
+            dict
+        """
+        return {
+            'name': self.name,
+            'headers': list(self.headers),
+            'results': [
+                dict(zip(self.headers, row))
+                for row in self.results
+                ]
+            }
+
